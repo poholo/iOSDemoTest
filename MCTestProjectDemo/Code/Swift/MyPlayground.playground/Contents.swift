8fc57b5b9803f4fa8f49a3851c710911ec6b93bb
poholo <mailjiancheng@163.com>
2021-07-28 18:39:18 +0800

    ..

--- MCTestProjectDemo/Code/Swift/MyPlayground.playground/Contents.swift
+++ MCTestProjectDemo/Code/Swift/MyPlayground.playground/Contents.swift
@@ -21,4 +21,13 @@ print(aa is A)
 print(bb is A)
 
 var bytes: [CChar] = [1, 2, 3]
-    
+
+var u: String = "http://baidu.com/大家看;领导看见/abc/aaa.json?param=data&s=的;打卡进度;放假啊;"
+import Foundation
+
+let url = u.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+print(url)
+
+let url2 = "http://img.gymchina.com/h5/pc/fs/专题管理/690x216.png".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+
+print(url2)
