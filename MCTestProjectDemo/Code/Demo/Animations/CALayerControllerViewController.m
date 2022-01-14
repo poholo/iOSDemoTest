d3dee36225f8e4856fd1aac8a585ee5b597c603a
poholo <mailjiancheng@163.com>
2022-01-14 10:38:59 +0800

    CGAffineTransform

--- MCTestProjectDemo/Code/Demo/Animations/CALayerControllerViewController.m
+++ MCTestProjectDemo/Code/Demo/Animations/CALayerControllerViewController.m
@@ -13,16 +13,36 @@
 @property(nonatomic, strong) UIView * shadowView;
 @property(nonatomic, strong) UIView * contentView;
 
+@property(nonatomic, assign) CGFloat manuY;
+@property(nonatomic, assign) CGFloat manuW;
+@property(nonatomic, assign) CGFloat manuH;
+@property(nonatomic, strong) UIButton *transformBtn;
+@property(nonatomic, strong) UIButton *transformYBtn;
+@property(nonatomic, strong) UIButton *transformScaleWBtn;
+@property(nonatomic, strong) UIButton *transformScaleHBtn;
+@property(nonatomic, strong) UIButton *transformbBtn;
+@property(nonatomic, strong) UIButton *transformcBtn;
+@property(nonatomic, strong) UIButton *scaleBtn;
+@property(nonatomic, strong) UIButton *invertBtn;
+@property(nonatomic, strong) UIButton *rotateBtn;
+@property(nonatomic, strong) UIButton *contactBtn;
+@property(nonatomic, strong) UIButton *resetBtn;
+
+
 @end
 
 @implementation CALayerControllerViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.title = @"CGAffineTransform";
+    self.manuY = CGRectGetHeight(self.view.frame) - 44 - 150;
+    self.manuW = CGRectGetWidth(self.view.frame) / 4;
+    self.manuH = 44;
     // Do any additional setup after loading the view.
     [self.view addSubview:self.shadowView];
     [self.shadowView addSubview:self.contentView];
-    self.shadowView.frame = CGRectMake(40, 100, 150 , 150);
+    self.shadowView.frame = CGRectMake(40, 100, 60 , 30);
     self.contentView.frame = self.shadowView.bounds;
     self.shadowView.backgroundColor = [UIColor redColor];
     self.shadowView.layer.shadowColor = [UIColor blueColor].CGColor;
@@ -30,7 +50,101 @@
     self.shadowView.layer.cornerRadius = 10;
     self.shadowView.layer.shadowOffset = CGSizeMake(0, 0);
     self.shadowView.layer.shadowOpacity = 1.0f;
+    
+    [self.view addSubview:self.transformBtn];
+    [self.view addSubview:self.transformYBtn];
+    [self.view addSubview:self.transformScaleWBtn];
+    [self.view addSubview:self.transformScaleHBtn];
+    [self.view addSubview:self.transformbBtn];
+    [self.view addSubview:self.transformcBtn];
+    [self.view addSubview:self.scaleBtn];
+    [self.view addSubview:self.resetBtn];
+    [self.view addSubview:self.invertBtn];
+    [self.view addSubview:self.rotateBtn];
+    [self.view addSubview:self.contactBtn];
+    
+    [self printCGTransform];
 
+}
+
+- (void)printCGTransform {
+    CGAffineTransform transform = self.shadowView.transform;
+    NSLog(@"\n a = %lf b = %lf \n c = %lf d = %lf \n tx = %lf ty = %lf", transform.a, transform.b, transform.c, transform.d, transform.tx, transform.ty);
+}
+
+- (void)xmov {
+    CGAffineTransform transform = self.shadowView.transform;
+    transform.tx += 10;
+    self.shadowView.transform = transform;
+    [self printCGTransform];
+}
+
+- (void)ymov {
+    CGAffineTransform transform = self.shadowView.transform;
+    transform.ty += 10;
+    self.shadowView.transform = transform;
+    [self printCGTransform];
+}
+
+- (void)scaleW {
+    CGAffineTransform transform = self.shadowView.transform;
+    transform.a += 0.1;
+    self.shadowView.transform = transform;
+    [self printCGTransform];
+}
+
+- (void)scaleH {
+    CGAffineTransform transform = self.shadowView.transform;
+    transform.d += 0.1;
+    self.shadowView.transform = transform;
+    [self printCGTransform];
+}
+
+- (void)bchange {
+    CGAffineTransform transform = self.shadowView.transform;
+    transform.b += 0.1;
+    self.shadowView.transform = transform;
+    [self printCGTransform];
+}
+
+- (void)cchange {
+    CGAffineTransform transform = self.shadowView.transform;
+    transform.c += 0.1;
+    self.shadowView.transform = transform;
+    [self printCGTransform];
+}
+
+- (void)scale {
+    CGAffineTransform transform = self.shadowView.transform;
+    CGAffineTransform finalTransform = CGAffineTransformScale(transform, 2, 3);
+    self.shadowView.transform = finalTransform;
+    [self printCGTransform];
+}
+
+- (void)invert {
+    CGAffineTransform transform = self.shadowView.transform;
+    CGAffineTransform finalTransform = CGAffineTransformInvert(transform);
+    self.shadowView.transform = finalTransform;
+    [self printCGTransform];
+}
+
+- (void)rotate {
+    CGAffineTransform transform = self.shadowView.transform;
+    CGAffineTransform finalTransform = CGAffineTransformRotate(transform, M_PI * 0.3);
+    self.shadowView.transform = finalTransform;
+    [self printCGTransform];
+}
+
+- (void)contact {
+    CGAffineTransform transform = self.shadowView.transform;
+    CGAffineTransform t2 = CGAffineTransformMake(0.9, 0.1, 0.7, 0.1, 10, 20);
+    CGAffineTransform finalTransform = CGAffineTransformConcat(transform, t2);
+    self.shadowView.transform = finalTransform;
+    [self printCGTransform];
+}
+
+- (void)reset {
+    self.shadowView.transform = CGAffineTransformIdentity;
 }
 
 - (UIView *)shadowView {
@@ -44,8 +158,154 @@
     if(!_contentView) {
         _contentView = [UIView new];
         _contentView.backgroundColor = [UIColor blackColor];
+        _contentView.layer.cornerRadius = 10;
+        _contentView.layer.borderColor = [UIColor orangeColor].CGColor;
+        _contentView.layer.borderWidth = 2;
     }
     return _contentView;
+}
+
+- (UIButton *)transformBtn {
+    if(!_transformBtn) {
+        _transformBtn = [UIButton new];
+        _transformBtn.frame = CGRectMake(0, self.manuY, self.manuW, self.manuH);
+        [_transformBtn setTitle:@"X mov" forState:UIControlStateNormal];
+        [_transformBtn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+        [_transformBtn addTarget:self action:@selector(xmov) forControlEvents:UIControlEventTouchUpInside];
+        _transformBtn.layer.borderColor = [UIColor orangeColor].CGColor;
+        _transformBtn.layer.borderWidth = 1;
+    }
+    return _transformBtn;
+}
+
+- (UIButton *)transformYBtn {
+    if(!_transformYBtn) {
+        _transformYBtn = [UIButton new];
+        _transformYBtn.frame = CGRectMake(self.manuW, self.manuY, self.manuW, self.manuH);
+        [_transformYBtn setTitle:@"Y mov" forState:UIControlStateNormal];
+        [_transformYBtn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+        [_transformYBtn addTarget:self action:@selector(ymov) forControlEvents:UIControlEventTouchUpInside];
+        _transformYBtn.layer.borderColor = [UIColor orangeColor].CGColor;
+        _transformYBtn.layer.borderWidth = 1;
+    }
+    return _transformYBtn;
+}
+
+- (UIButton *)transformScaleWBtn {
+    if(!_transformScaleWBtn) {
+        _transformScaleWBtn = [UIButton new];
+        _transformScaleWBtn.frame = CGRectMake(2 * self.manuW, self.manuY, self.manuW, self.manuH);
+        [_transformScaleWBtn setTitle:@"scaleW" forState:UIControlStateNormal];
+        [_transformScaleWBtn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+        [_transformScaleWBtn addTarget:self action:@selector(scaleW) forControlEvents:UIControlEventTouchUpInside];
+        _transformScaleWBtn.layer.borderColor = [UIColor orangeColor].CGColor;
+        _transformScaleWBtn.layer.borderWidth = 1;
+    }
+    return _transformScaleWBtn;
+}
+
+- (UIButton *)transformScaleHBtn {
+    if(!_transformScaleHBtn) {
+        _transformScaleHBtn = [UIButton new];
+        _transformScaleHBtn.frame = CGRectMake(3 * self.manuW, self.manuY, self.manuW, self.manuH);
+        [_transformScaleHBtn setTitle:@"scaleH" forState:UIControlStateNormal];
+        [_transformScaleHBtn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+        [_transformScaleHBtn addTarget:self action:@selector(scaleH) forControlEvents:UIControlEventTouchUpInside];
+        _transformScaleHBtn.layer.borderColor = [UIColor orangeColor].CGColor;
+        _transformScaleHBtn.layer.borderWidth = 1;
+    }
+    return _transformScaleHBtn;
+}
+
+- (UIButton *)transformbBtn {
+    if(!_transformbBtn) {
+        _transformbBtn = [UIButton new];
+        _transformbBtn.frame = CGRectMake(0 * self.manuW, self.manuY + self.manuH, self.manuW, self.manuH);
+        [_transformbBtn setTitle:@"b change" forState:UIControlStateNormal];
+        [_transformbBtn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+        [_transformbBtn addTarget:self action:@selector(bchange) forControlEvents:UIControlEventTouchUpInside];
+        _transformbBtn.layer.borderColor = [UIColor orangeColor].CGColor;
+        _transformbBtn.layer.borderWidth = 1;
+    }
+    return _transformbBtn;
+}
+
+- (UIButton *)transformcBtn {
+    if(!_transformcBtn) {
+        _transformcBtn = [UIButton new];
+        _transformcBtn.frame = CGRectMake(1 * self.manuW, self.manuY + self.manuH, self.manuW, self.manuH);
+        [_transformcBtn setTitle:@"c change" forState:UIControlStateNormal];
+        [_transformcBtn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+        [_transformcBtn addTarget:self action:@selector(cchange) forControlEvents:UIControlEventTouchUpInside];
+        _transformcBtn.layer.borderColor = [UIColor orangeColor].CGColor;
+        _transformcBtn.layer.borderWidth = 1;
+    }
+    return _transformcBtn;
+}
+
+- (UIButton *)scaleBtn {
+    if(!_scaleBtn) {
+        _scaleBtn = [UIButton new];
+        _scaleBtn.frame = CGRectMake(2 * self.manuW, self.manuY + self.manuH, self.manuW, self.manuH);
+        [_scaleBtn setTitle:@"scale" forState:UIControlStateNormal];
+        [_scaleBtn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+        [_scaleBtn addTarget:self action:@selector(scale) forControlEvents:UIControlEventTouchUpInside];
+        _scaleBtn.layer.borderColor = [UIColor orangeColor].CGColor;
+        _scaleBtn.layer.borderWidth = 1;
+    }
+    return _scaleBtn;
+}
+
+- (UIButton *)invertBtn {
+    if(!_invertBtn) {
+        _invertBtn = [UIButton new];
+        _invertBtn.frame = CGRectMake(3 * self.manuW, self.manuY + self.manuH, self.manuW, self.manuH);
+        [_invertBtn setTitle:@"invert" forState:UIControlStateNormal];
+        [_invertBtn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+        [_invertBtn addTarget:self action:@selector(invert) forControlEvents:UIControlEventTouchUpInside];
+        _invertBtn.layer.borderColor = [UIColor orangeColor].CGColor;
+        _invertBtn.layer.borderWidth = 1;
+    }
+    return _invertBtn;
+}
+
+- (UIButton *)rotateBtn {
+    if(!_rotateBtn) {
+        _rotateBtn = [UIButton new];
+        _rotateBtn.frame = CGRectMake(0 * self.manuW, self.manuY + 2 * self.manuH, self.manuW, self.manuH);
+        [_rotateBtn setTitle:@"rotate" forState:UIControlStateNormal];
+        [_rotateBtn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+        [_rotateBtn addTarget:self action:@selector(rotate) forControlEvents:UIControlEventTouchUpInside];
+        _rotateBtn.layer.borderColor = [UIColor orangeColor].CGColor;
+        _rotateBtn.layer.borderWidth = 1;
+    }
+    return _rotateBtn;
+}
+
+- (UIButton *)contactBtn {
+    if(!_contactBtn) {
+        _contactBtn = [UIButton new];
+        _contactBtn.frame = CGRectMake(1 * self.manuW, self.manuY + 2 * self.manuH, self.manuW, self.manuH);
+        [_contactBtn setTitle:@"contact" forState:UIControlStateNormal];
+        [_contactBtn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+        [_contactBtn addTarget:self action:@selector(contact) forControlEvents:UIControlEventTouchUpInside];
+        _contactBtn.layer.borderColor = [UIColor orangeColor].CGColor;
+        _contactBtn.layer.borderWidth = 1;
+    }
+    return _contactBtn;
+}
+
+- (UIButton *)resetBtn {
+    if(!_resetBtn) {
+        _resetBtn = [UIButton new];
+        _resetBtn.frame = CGRectMake(3  * self.manuW, self.manuY + self.manuH * 2, self.manuW, self.manuH);
+        [_resetBtn setTitle:@"reset" forState:UIControlStateNormal];
+        [_resetBtn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+        [_resetBtn addTarget:self action:@selector(reset) forControlEvents:UIControlEventTouchUpInside];
+        _resetBtn.layer.borderColor = [UIColor orangeColor].CGColor;
+        _resetBtn.layer.borderWidth = 1;
+    }
+    return _resetBtn;
 }
 
 @end
--- MCTestProjectDemo/Code/Swift/MyViewVC.playground/Contents.swift
+++ MCTestProjectDemo/Code/Swift/MyViewVC.playground/Contents.swift
@@ -19,3 +19,53 @@ class MyViewController : UIViewController {
 }
 // Present the view controller in the Live View window
 PlaygroundPage.current.liveView = MyViewController()
+
+
+
+let a = UIView()
+let b = UIView()
+
+if a != b {
+    print("a == b")
+}
+
+if a !== b {
+    print("a === b")
+}
+
+
+
+"""
+properties
+"""
+
+
+struct Shop {
+    var eggs: Int
+    let price: Int
+}
+
+let shop = Shop(eggs: 10, price: 4)
+
+print(shop)
+
+//shop.eggs = 9 err
+
+
+var sp = Shop(eggs: 11, price: 4)
+sp.eggs = 9
+
+
+class CShop {
+    var eddgs: Int
+    let price: Int
+    
+    init(_ e: Int, p: Int) {
+        eddgs = e
+        price = p
+    }
+}
+
+let cshop = CShop(10, p: 4)
+cshop.eddgs = 11
+print(cshop.eddgs)
